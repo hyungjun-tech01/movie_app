1cdcf4bfe20d77b0a54ea5e4dae6dac164c80cc1
hyungjun-tech01 <whmoon00@naver.com>
2022-12-29 11:48:27 +0900

    react route dom v6 한계 확인

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,12 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
 
+// v6에서는 더이상 Link to 를 통해 state/props 전달은 할 수 없다. 따라서 아래 기능은 동작하지 않는다. 
+// custom hook 을 만들어서 사용하라고 되어 있는데 , hook 은 아직 공부 못했다. 다음에 공부하게 되면 추가할 예정 
+
 function Movie({id, title, year, summary, poster, genres}){
     return (
     <Link to={{
         pathname:"/movie_detail",
         state:{
-            summary
+            summary,
+            title,
+            poster,
         }
     }}>
         <div className="movie">
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,15 @@
 import React from "react"
-function Detail(props){
-    console.log(props);
-    return(
-        <span>hello</span>
-    )
+// react route dom v6에서는 더이상 Link to 를 통해 state/props 전달은 할 수 없다. 따라서 아래 기능은 동작하지 않는다. 
+// custom hook 을 만들어서 사용하라고 되어 있는데 , hook 은 아직 공부 못했다. 다음에 공부하게 되면 추가할 예정 
+class Detail extends React.Component{ 
+    componentDidMount(){
+        const {location, history} = this.props;
+        if(location.state === undefined )
+            history.push("/");
+    }
+    render(){
+        return <span> Hello</span>
+    }
+
 }
 export default Detail;
